1927155ad6b07a36473922b945884a6f26b80555
Abdelfatah Saad <120993266+abdoooooo90@users.noreply.github.com>
2025-09-01 23:48:21 +0300

    Add project files.

---
+++ E-LapShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_LapShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
