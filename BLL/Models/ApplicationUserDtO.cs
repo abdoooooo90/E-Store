386fc1aadd2c431d6204f5a1cb0e4d268f48618d
moazzehry <110047752+moazzehry@users.noreply.github.com>
2025-09-03 15:04:29 +0300

    create DtO

---
+++ BLL/Models/ApplicationUserDtO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    internal class ApplicationUserDTO
+    {
+
+        public string Id { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+
+        // public List<OrderDto> Orders { get; set; } = new();
+        // public List<CartItemDto> CartItems { get; set; } = new();
+    }
+}
---
+++ BLL/Models/PaymentWithOrderDtO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    internal class PaymentWithOrderDtO
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public string Status { get; set; } = null!;
+
+       
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal OrderTotal { get; set; }
+    
+    }
+}
